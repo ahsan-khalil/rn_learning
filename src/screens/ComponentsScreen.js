188dfbdc7b14a733eacd0aa0043da7d8d8b561c0
Ahsan Khalil <ahsan.khalil@arbisoft.com>
2022-10-11 19:10:34 +0500

    react part 2 done

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,14 +1,24 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
+
+const name = "Muhammad Ahsan Khalil";
 
 const ComponentsScreen = () => {
-  return <Text style={styles.textStyle}>This is the components screen</Text>;
+  return (
+          <View>
+            <Text style={styles.textStyle}>Getting Started with react native</Text>
+            <Text style={styles.SubHeaderStyle}>My name is { name }</Text>
+          </View>
+        ); 
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 40,
+    fontSize: 45,
   },
+  SubHeaderStyle: {
+    fontSize: 20
+  }
 });
 
 export default ComponentsScreen;
