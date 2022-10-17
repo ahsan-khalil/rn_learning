bbb06f78a3fd586c8d8078f1c77ef07786b6d7c9
Ahsan Khalil <ahsan.khalil@arbisoft.com>
2022-10-17 18:40:17 +0500

    lesson 7 layouts

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,21 +3,49 @@ import { Text, View, StyleSheet } from 'react-native'
 
 const BoxScreen = props => {
     return <View style={styles.viewStyle}>
-        <Text style={styles.textStyle}>Box Screen</Text>
+        <Text style={styles.textStyleOne}>Box Screen</Text>
+        <Text style={styles.textStyleTwo}>Box Screen</Text>
+        <Text style={styles.textStyleThree}>Box Screen</Text>
     </View>
 };
 
 const styles = StyleSheet.create({
     textStyle: {
         fontSize: 30,
-        margin: 20,
         color: "red",
         borderColor: "red",
         borderWidth: 5
     },
+    textStyleOne: {
+        fontSize: 15,
+        color: "red",
+        borderColor: "red",
+        borderWidth: 5,
+        flex: 2
+    },
+    textStyleTwo: {
+        fontSize: 15,
+        color: "green",
+        borderColor: "green",
+        borderWidth: 5,
+        alignSelf: 'center',
+        flex: 3,
+    },
+    textStyleThree: {
+        fontSize: 15,
+        color: "blue",
+        borderColor: "blue",
+        borderWidth: 5,
+        flex: 2,
+        // ...StyleSheet.absoluteFillObject
+    },
     viewStyle: {
         borderWidth: 1,
-        margin: 2
+        margin: 2,
+        alignItems: 'stretch',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        height: 300
     }
 });
 
